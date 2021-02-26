ef976e54d7dc1cda24bc52926fc6576f9f3568c2
Andrea Smetankova <andrea.smetankova@student.tuke.sk>
2021-02-26 15:22:35 +0100

    thread with functions with multiple arguments

--- test.c
+++ test.c
@@ -3,16 +3,31 @@
 #include <unistd.h>
 #include <pthread.h>
 
-void * myThreadFun(void* vargp){
+
+
+struct argument_struct{
+    int a;
+    int b;
+    int* array;
+};
+
+void * myThreadFun(void * arguments){
     sleep(1);
-    printf("Printing from my thread\n");
+    struct argument_struct *args = (struct argument_struct *)arguments;
+    printf("Printing from my thread %d %d\n",args->a,args->b);
+
     return NULL;
 }
 
 int main(){
+    struct argument_struct arguments;
+    int pole[] = {1,2,3,4};
+    arguments.a = 1;
+    arguments.b = 2;
+    arguments.array = pole;
     pthread_t thread_id;
     printf("Before thread\n");
-    pthread_create(&thread_id,NULL,myThreadFun, NULL);
+    pthread_create(&thread_id,NULL,(void*)myThreadFun, (void*)&arguments);
     pthread_join(thread_id,NULL);
     printf("After thread\n");
 
