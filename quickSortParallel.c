715ae79baf8f633f5743f22898ba21777c9da772
Andrea Smetankova <andrea.smetankova@student.tuke.sk>
2021-03-04 09:33:55 +0100

    little steps so close to success I hope

--- quickSortParallel.c
+++ quickSortParallel.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 struct arrays {
     int p0_start;
@@ -11,34 +12,85 @@ struct arrays {
     int overall_size;
 };
 
-int step = 1;
+struct arguments{
+    int* array;
+    int start;
+    int end;
+    int proces_id;
+};
 
+int step = 1;
+pthread_mutex_t mutex;
 void printArray(int array[],int size);
 struct arrays divideArray(int* array, int size);
 int partition (int left, int right, int array[]);
 void quicksort (int left, int right, int array[]);
 void swap(int* a, int* b);
-void sortArray(int* array, int start, int end);
+void sortArray(void* arguments);
 
 
 int main(int argc, const char* argv[]) {
 
     int array[] = {75,91,15,64,21,8,88,54,50,12,47,72,65,54,66,22,83,66,67,0,70,98,99,82,20,40,89,47,19,61,86,85};
     int size = sizeof(array) / sizeof(array[0]);
+    pthread_t thread_id0,thread_id1,thread_id2,thread_id3;
+    struct arrays divided_array = divideArray(array,size);
+    struct arguments arg0,arg1,arg2,arg3;
+    
+
+    arg0.array = array;
+    arg0.start = divided_array.p0_start;
+    arg0.end = divided_array.p0_start + divided_array.part_size;
+    arg0.proces_id = 0;
+
+    arg1.array = array;
+    arg1.start = divided_array.p1_start;
+    arg1.end = divided_array.p1_start + divided_array.part_size;
+    arg1.proces_id = 1;
+
+    arg2.array = array;
+    arg2.start = divided_array.p2_start;
+    arg2.end = divided_array.p2_start + divided_array.part_size;
+    arg2.proces_id = 2;
+
+    arg3.array = array;
+    arg3.start = divided_array.p3_start;
+    arg3.end = divided_array.p3_start + divided_array.part_size;
+    arg3.proces_id = 3;
+
+
+    for(int i = 0; i < 2; i++)
+    {
+        pthread_create(&thread_id0,NULL,(void*)sortArray, (void*)&arg0);
+        pthread_create(&thread_id1,NULL,(void*)sortArray, (void*)&arg1);
+        pthread_create(&thread_id2,NULL,(void*)sortArray, (void*)&arg2);
+        pthread_create(&thread_id3,NULL,(void*)sortArray, (void*)&arg3);
+        pthread_join(thread_id0,NULL);
+        pthread_join(thread_id1,NULL);
+        pthread_join(thread_id2,NULL);
+        pthread_join(thread_id3,NULL);
+        printf("After threads %i\n", i + 1);
+        step++;
+        
+    }
+    
+
     printf("Unsorted array: ");
     printArray(array,size);
     printf("\n"); 
    
-    struct arrays divided_array = divideArray(array,size);
+   
     // printf("Sorted array: ");
-    // printArray(array,size);
-    printf("first part of the array:");
-    printArray(array,8);
-    printf("\n");
-    quicksort(divided_array.p0_start,divided_array.part_size,array);
-    printf("first part of the array sorted:");
-    printArray(array,8);
-    printf("\n");
+    // // printArray(array,size);
+    // printf("first part of the array:");
+    // printArray(array,8);
+    // printf("\n");
+    // quicksort(divided_array.p0_start,divided_array.part_size,array);
+    // printf("first part of the array sorted:");
+    // printArray(array,8);
+    // printf("\n");
+
+
     printf("whole array:");
     printArray(array,size);
     printf("\n");
@@ -50,12 +102,28 @@ int main(int argc, const char* argv[]) {
 
 }
 
-void sortArray(int* array, int start, int end, int proces_id){
-    quicksort(start,end,array);
-
-    if((proces_id == 0) && (step == 1))
+void sortArray(void * arguments){
+    struct arguments *args = (struct arguments *) arguments;
+    quicksort(args->start, args->end, args->array);
+    
+    if (step == 1)
     {
-        
+        if(args->proces_id == 0)
+        {
+           printf("1. proces 0 and expected step 1 acctual step %i \n", step);  
+        } else
+        {
+            printf("2. proces %i and expected step 1 acctual step %i\n",args->proces_id, step);
+        }
+    }else if(step == 2)
+    {
+        if(args->proces_id == 0 || args->proces_id == 2)
+        {
+            printf("3. proces %i and expected step 2 acctual step %i\n", args->proces_id, step);
+        }else
+        {
+            printf("4. proces %i and expected step 2 acctual step %i\n", args->proces_id, step);
+        }
     }
 }
 
