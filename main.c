462f29f14950a2d78018a59c5f5116062e5868a7
Andrea Smetankova <andrea.smetankova@student.tuke.sk>
2021-03-27 22:48:31 +0100

    skraslovanie

--- main.c
+++ main.c
@@ -12,23 +12,28 @@ void generateNumArray(int size, int* array);
 
 int main(int argc, const char* argv[]) {
 
-    int array[] = {75,91,15,64,21,8,88,54,50,12,47,72,65,54,66,22,83,66,67,0,70,98,99,82,20,40,89,47,19,61,86,85};
-    int array2[] = {75,91,15,64,21,8,88,54,50,12,47,72,65,54,66,22,83,66,67,0,70,98,99,82,20,40,89,47,19,61,86,85};
-    int array3[] = {75,91,15,64,21,8,88,54,50,12,47,72,65,54,66,22,83,66,67,0,70,98,99,82,20,40,89,47,19,61,86,85};
-    int array4[] = {75,91,15,64,21,8,88,54,50,12,47,72,65,54,66,22,83,66,67,0,70,98,99,82,20,40,89,47,19,61,86,85};
-    int size = sizeof(array) / sizeof(array[0]);
+    // int array[] = {75,91,15,64,21,8,88,54,50,12,47,72,65,54,66,22,83,66,67,0,70,98,99,82,20,40,89,47,19,61,86,85};
+    // int array2[] = {75,91,15,64,21,8,88,54,50,12,47,72,65,54,66,22,83,66,67,0,70,98,99,82,20,40,89,47,19,61,86,85};
+    // int array3[] = {75,91,15,64,21,8,88,54,50,12,47,72,65,54,66,22,83,66,67,0,70,98,99,82,20,40,89,47,19,61,86,85};
+    // int array4[] = {75,91,15,64,21,8,88,54,50,12,47,72,65,54,66,22,83,66,67,0,70,98,99,82,20,40,89,47,19,61,86,85};
+    // int size = sizeof(array) / sizeof(array[0]);
     // int size2 = sizeof(array2) / sizeof(array2[0]);
-    // generateNumFile(1000,"numbers.txt");
-    // int size = 100000;
-    // int array[size];
-    // int array2[size];
-    // generateNumArray(size, array);
-    // copyArray(array, array2, size, 0);
+    int size = 100000;
+    int array[size];
+    int array2[size];
+    int array3[size];
+    int array4[size];
+    generateNumArray(size, array);
+    copyArray(array, array2, size, 0);
+    copyArray(array, array3, size, 0);
+    copyArray(array, array4, size, 0);
     
+    printf("ARRAY SIZE = %i \n", size);
+    printf("Unsorted array: ");
+    // printArray(array,size);
 
     printf("-------------------------------------QUICKSORT-------------------------\n");
-    printf("Unsorted array: ");
-    printArray(array,size);
+    
     printf("\n");
 
     clock_t t; 
@@ -41,16 +46,12 @@ int main(int argc, const char* argv[]) {
 
 
     printf("Sorted array: ");
-    printArray(array,size);
+    // printArray(array,size);
     printf("\n");
-    printf("-------------------------------------QUICKSORT-------------------------\n");
+    printf("---------------------------------------------------------------------\n");
 
 
     printf("\n-------------------------------------PARALLEL 2 threads QUICKSORT-------------------------\n");
-    printf("Unsorted array: ");
-    printArray(array2,size);
-    printf("\n");
-
     clock_t t2; 
     t2 = clock();
     parallelQuickSort_2threads(array2,size);
@@ -60,15 +61,11 @@ int main(int argc, const char* argv[]) {
     printf("parallelQuickSort() with 2 threads took %f seconds to execute \n", time_taken2);
 
     printf("Sorted array: ");
-    printArray(array2,size);
+    // printArray(array2,size);
     printf("\n");
-    printf("-------------------------------------PARALLEL 2 threads QUICKSORT-------------------------\n");
+    printf("-----------------------------------------------------------------------------------------------\n");
 
     printf("\n-------------------------------------PARALLEL 4 threads QUICKSORT-------------------------\n");
-    printf("Unsorted array: ");
-    printArray(array3,size);
-    printf("\n");
-
     clock_t t3; 
     t3 = clock();
     parallelQuickSort_4threads(array3,size);
@@ -78,15 +75,11 @@ int main(int argc, const char* argv[]) {
     printf("parallelQuickSort() with 4 threads took %f seconds to execute \n", time_taken3);
 
     printf("Sorted array: ");
-    printArray(array3,size);
+    // printArray(array3,size);
     printf("\n");
-    printf("-------------------------------------PARALLEL 4 threads QUICKSORT-------------------------\n");
+    printf("---------------------------------------------------------------------------------------------\n");
 
     printf("\n-------------------------------------PARALLEL 8 threads QUICKSORT-------------------------\n");
-    printf("Unsorted array: ");
-    printArray(array4,size);
-    printf("\n");
-
     clock_t t4; 
     t4 = clock();
     parallelQuickSort_8threads(array4,size);
@@ -96,9 +89,9 @@ int main(int argc, const char* argv[]) {
     printf("parallelQuickSort() with 8 threads took %f seconds to execute \n", time_taken4);
 
     printf("Sorted array: ");
-    printArray(array4,size);
+    // printArray(array4,size);
     printf("\n");
-    printf("-------------------------------------PARALLEL 8 threads QUICKSORT-------------------------\n");
+    printf("------------------------------------------------------------------------------------------------\n");
 
     return 0;
     
